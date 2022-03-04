edf62a35a66126527a33896d295a91ac29b11b9d
clrtyjones <jdm.johnz@gmail.com>
2022-03-03 17:56:26 -0800

    🔧 - 3/3 One Added Amount Implemented.

--- main.js
+++ main.js
@@ -9,24 +9,28 @@ let images = ["./src_images/dice-01.svg",
 // ID's For Query
 let leftDie = document.querySelectorAll("#die-1");
 let rightDie = document.querySelectorAll("#die-2");
-let leftRolled = document.getElementById("rolledNum");
-let rightRolled = document.getElementById("rightRolledNum");
-var checkBox = document.getElementById("multiplyClicked");
+let rolled = document.getElementById("rolledNum");
+var checkBox = document.getElementById("leftVote");
 
 // Global Variables For L/R Values + Multiply Flag
 let leftValue = 0;
 let rightValue = 0;
-let multiplier = false;
+let leftVote = false;
+let rightVote = false;
 
 // Default Left Die
 // - startup value = 0
 document.querySelector("#die-1").setAttribute("src", images[Math.floor(Math.random()*6)]);
-document.querySelector("#leftTotal").innerHTML = "Total: " + leftValue;
+document.querySelector("#leftTotal").innerHTML = "Total: <br>" + leftValue;
 
 // Default Right Die
 // - startup value = 0
 document.querySelector("#die-2").setAttribute("src", images[Math.floor(Math.random()*6)]);
-document.querySelector("#rightTotal").innerHTML = "Total: " + rightValue;
+document.querySelector("#rightTotal").innerHTML = "Total: <br>" + rightValue;
+
+// Default Rolled Num
+// - needed for formatting.
+document.querySelector("#rolledNum").innerHTML = "Added 0!";
 
 
 /*
@@ -38,9 +42,9 @@ document.querySelector("#rightTotal").innerHTML = "Total: " + rightValue;
 function multiply()
 {
     if (checkBox.checked == true) {
-        multiplier = true;
+        leftVote = true;
     } else {
-        multiplier = false;
+        leftVote = false;
     }
 }
 
@@ -80,20 +84,37 @@ function diceRoll()
         document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
         document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
 
-        // Check Multiplier + Update Total
-        if (multiplier) {
-            dieOneValue = (dieOneValue + 1) * 10;
-            dieTwoValue = (dieTwoValue + 1) * 10;
+        // Add Total Amount To Corresponding Vote
+        if (leftVote) {
+
+            // Update Value Of Left Side
+            leftValue += ((dieOneValue + 1) + (dieTwoValue + 1));
+            document.querySelector("#leftTotal").innerHTML = "Total: <br>" + leftValue;
+
         } else {
-            dieOneValue += 1;
-            dieTwoValue += 1;
+
+            // Update Value Of Right Side
+            rightValue += ((dieOneValue + 1) + (dieTwoValue + 1));
+            document.querySelector("#rightTotal").innerHTML = "Total: <br>" + rightValue;
+
         }
 
-        leftValue += dieOneValue;
-        rightValue += dieTwoValue;
-        document.querySelector("#leftTotal").innerHTML = "Total: " + leftValue;
-        document.querySelector("#rightTotal").innerHTML = "Total: " + rightValue;
+        // TODO: Merge Amount Added Text Into One
+        // TODO: Implement A New Multiply Method
 
+        // Display Output According To Multiplier
+        // ! Multiplier Not Implemented Yet.
+        rolled.classList.add("fade-in");
+        document.querySelector("#rolledNum").innerHTML = 
+                "Added " + ((dieOneValue + 1) + (dieTwoValue + 1)) + "!";
+
+        setTimeout(function () {
+            rolled.classList.remove("fade-in");
+        }, 1500);
+
+        // **MERGE THESE TWO TOGETHER
+
+        /*
         leftRolled.classList.add("fade-in");
         rightRolled.classList.add("fade-in");
 
@@ -117,62 +138,8 @@ function diceRoll()
             leftRolled.classList.remove("fade-in");
             rightRolled.classList.remove("fade-in");
         }, 1500);
-    },
-    1000
-    );
-}
 
-
-/*
-*   Right Die Roll Function - Generates Random (1-6) Then Adds Onto Right Total.
-*
-*   @return     Updated texts/variables for right side + what was added.
-*   @see        Updated total, what was added.
-*/
-function dieRoll_Two() 
-{
-    // Add Shake To Right Die
-    rightDie.forEach(function(die){
-        die.classList.add("shake");
-    });
-
-    // Remove Shake To Right Die + Calc. Amount
-    setTimeout(function(){
-        rightDie.forEach(function(die){
-            die.classList.remove("shake");
-        });
-
-        // Generate Random Die Roll (1-6)
-        let dieTwoValue = Math.floor(Math.random()*6);
-        console.log(dieTwoValue);
-
-        // Update Die Image + Total
-        document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
-
-        // Check Multiplier + Update Total
-        if (multiplier) {
-            dieTwoValue = (dieTwoValue + 1) * 10;
-        } else {
-            dieTwoValue += 1;
-        }
-
-        rightValue += dieTwoValue;
-        document.querySelector("#rightTotal").innerHTML = "Total: " + rightValue;
-
-        rightRolled.classList.add("fade-in");
-
-        // Display Output According To Multiplier
-        if (multiplier) {
-            document.querySelector("#rightRolledNum").innerHTML = 
-                "10x Multiplier! " +
-                "Added " + dieTwoValue + "!";
-        } else {
-            document.querySelector("#rightRolledNum").innerHTML = 
-                "Added " + dieTwoValue + "!";
-        }
-        setTimeout(function () {
-            rightRolled.classList.remove("fade-in");
-        }, 1500);
+        */
     },
     1000
     );

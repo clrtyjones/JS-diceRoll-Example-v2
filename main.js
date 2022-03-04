00089b82799023575f4ecc398a9ec0ac67ee174c
clrtyjones <jdm.johnz@gmail.com>
2022-03-03 17:09:24 -0800

    🔧 - 3/3 One Button Implemented

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let images = ["./src_images/dice-01.svg",
 // ID's For Query
 let leftDie = document.querySelectorAll("#die-1");
 let rightDie = document.querySelectorAll("#die-2");
-let leftRolled = document.getElementById("leftRolledNum");
+let leftRolled = document.getElementById("rolledNum");
 let rightRolled = document.getElementById("rightRolledNum");
 var checkBox = document.getElementById("multiplyClicked");
 
@@ -46,54 +46,76 @@ function multiply()
 
 
 /*
-*   Left Die Roll Function - Generates Random (1-6) Then Adds Onto Left Total.
+*   Dice Roll Function - Generates Random (1-6) Then Adds Onto Corresponding Total.
 *
-*   @return     Updated texts/variables for left side + what was added.
+*   @return     Updated texts/variables for corresponding total + what was added.
 *   @see        Updated total, what was added.
 */
-function dieRoll_One() 
+function diceRoll() 
 {
-    // Add Shake To Left Die
+    // Add Shake To Left + Right Die
     leftDie.forEach(function(die){
         die.classList.add("shake");
     });
 
-    // Remove Shake To Left Die + Calc. Amount
+    rightDie.forEach(function(die) {
+        die.classList.add("shake");
+    })
+
+    // Remove Shake To Left/Right Die + Calc. Amount
     setTimeout(function(){
         leftDie.forEach(function(die){
             die.classList.remove("shake");
         });
-        
-        // Generate Random Die Roll (1-6)
-        let dieOneValue = Math.floor(Math.random()*6);
-        console.log(dieOneValue);
 
-        // Update Die Image
+        rightDie.forEach(function(die) {
+            die.classList.remove("shake");
+        })
+        
+        // Generate Random Dice Roll (1-6)
+        let dieOneValue = Math.floor(Math.random()*6);
+        let dieTwoValue = Math.floor(Math.random()*6);
+
+        // Update Dice Image
         document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
+        document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
 
         // Check Multiplier + Update Total
         if (multiplier) {
             dieOneValue = (dieOneValue + 1) * 10;
+            dieTwoValue = (dieTwoValue + 1) * 10;
         } else {
             dieOneValue += 1;
+            dieTwoValue += 1;
         }
 
         leftValue += dieOneValue;
+        rightValue += dieTwoValue;
         document.querySelector("#leftTotal").innerHTML = "Total: " + leftValue;
+        document.querySelector("#rightTotal").innerHTML = "Total: " + rightValue;
 
         leftRolled.classList.add("fade-in");
+        rightRolled.classList.add("fade-in");
 
         // Display Output According To Multiplier
         if (multiplier) {
-            document.querySelector("#leftRolledNum").innerHTML = 
+            document.querySelector("#rolledNum").innerHTML = 
                 "10x Multiplier! " +
                 "Added " + dieOneValue + "!";
+
+            document.querySelector("#rightRolledNum").innerHTML = 
+                "10x Multiplier!" + 
+                "Added " + dieTwoValue + "!";
         } else {
-            document.querySelector("#leftRolledNum").innerHTML = 
+            document.querySelector("#rolledNum").innerHTML = 
                 "Added " + dieOneValue + "!";
+
+            document.querySelector("#rightRolledNum").innerHTML = 
+                "Added " + dieTwoValue + "!";
         }
         setTimeout(function () {
             leftRolled.classList.remove("fade-in");
+            rightRolled.classList.remove("fade-in");
         }, 1500);
     },
     1000

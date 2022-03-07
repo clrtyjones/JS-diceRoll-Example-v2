e0c979e7905f2ab74be31cef483a87ee73125290
clrtyjones <jdm.johnz@gmail.com>
2022-03-06 18:01:53 -0800

    🔧 - 3/6 Functional Voting Implemented, NFT Images

--- main.js
+++ main.js
@@ -7,20 +7,15 @@ let images = ["./src_images/dice-01.svg",
 "./src_images/dice-06.svg"];
 
 // Import NFT Images -> Array
-let nftImage = ["./nft_examples/apeExample.jpg",
-"./nft_examples/dipExample.png",
-"./nft_examples/friendsExample.gif",
-"./nft_examples/hape2Example.png",
-"./nft_examples/palExample.png",
-"./nft_examples/punkExample.jpg",
-"./nft_examples/takashiExample.png",
-"./nft_examples/hapeExample.png"];
+let nftImage = ["./nft_examples/don-toretto.png",
+"./nft_examples/easy-e.png"];
 
 // ID's For Query
 let leftDie = document.querySelectorAll("#die-1");
 let rightDie = document.querySelectorAll("#die-2");
 let rolled = document.getElementById("rolledNum");
 var checkBox = document.getElementById("leftVote");
+const radioButtons = document.querySelectorAll('input[name="select"]');
 
 // Global Variables For L/R Values + Multiply Flag
 let leftValue = 0;
@@ -45,22 +40,9 @@ document.querySelector("#rightTotal").innerHTML = "Total: <br>" + rightValue;
 // - needed for formatting.
 document.querySelector("#rolledNum").innerHTML = "Added 0!";
 
-// Display Random Comptetitor NFT's
-function displayNFTs() {
-
-    // Base Case - No Pairs
-    if (document.querySelector("#left-nft").getAttribute() == nftImage[1]) {
-        console.log("YOOOOO");
-
-    }
-
-    // Generate Random Pair
-    document.querySelector("#left-nft").setAttribute("src", nftImage[Math.floor(Math.random()*8)]);
-    document.querySelector("#right-nft").setAttribute("src", nftImage[Math.floor(Math.random()*8)]);
-
-    // Check For Duplicates
-}
-displayNFTs();
+// Display Example NFTs
+document.querySelector("#left-nft").setAttribute("src", nftImage[1]);
+document.querySelector("#right-nft").setAttribute("src", nftImage[0]);
 
 /*
 *   Multiply Function - Applies A 10x Multiplier To The Added Value.
@@ -118,21 +100,32 @@ function diceRoll()
         document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
 
         // Add Total Amount To Corresponding Vote
-        if (leftVote) {
+        for (const radioButton of radioButtons) {
+            
+            // Check Which Side To Vote
+            if (radioButton.checked && radioButton.value == "left"){
 
-            // Update Value Of Left Side
-            leftValue += ((dieOneValue + 1) + (dieTwoValue + 1));
-            document.querySelector("#leftTotal").innerHTML = "Total: <br>" + leftValue;
+                // Update Value Of Left Side
+                leftValue += ((dieOneValue + 1) + (dieTwoValue + 1));
+                document.querySelector("#leftTotal").innerHTML = "Total: <br>" + leftValue;
 
-        } else {
+            } else if (radioButton.checked && radioButton.value == "right"){
 
-            // Update Value Of Right Side
-            rightValue += ((dieOneValue + 1) + (dieTwoValue + 1));
-            document.querySelector("#rightTotal").innerHTML = "Total: <br>" + rightValue;
+                // Update Value Of Right Side
+                rightValue += ((dieOneValue + 1) + (dieTwoValue + 1));
+                document.querySelector("#rightTotal").innerHTML = "Total: <br>" + rightValue;
 
+            } else { // No Vote Selected ... Should Not Occur W/ Radio Buttons
+                rolled.classList.add("fade-in");
+                document.querySelector("#rolledNum").innerHTML = 
+                    "Please Make A Vote Selection!";
+
+                setTimeout(function () {
+                    rolled.classList.remove("fade-in");
+                }, 1500);
+            }
         }
 
-        // TODO: Merge Amount Added Text Into One
         // TODO: Implement A New Multiply Method
 
         // Display Output According To Multiplier
@@ -144,35 +137,6 @@ function diceRoll()
         setTimeout(function () {
             rolled.classList.remove("fade-in");
         }, 1500);
-
-        // **MERGE THESE TWO TOGETHER
-
-        /*
-        leftRolled.classList.add("fade-in");
-        rightRolled.classList.add("fade-in");
-
-        // Display Output According To Multiplier
-        if (multiplier) {
-            document.querySelector("#rolledNum").innerHTML = 
-                "10x Multiplier! " +
-                "Added " + dieOneValue + "!";
-
-            document.querySelector("#rightRolledNum").innerHTML = 
-                "10x Multiplier!" + 
-                "Added " + dieTwoValue + "!";
-        } else {
-            document.querySelector("#rolledNum").innerHTML = 
-                "Added " + dieOneValue + "!";
-
-            document.querySelector("#rightRolledNum").innerHTML = 
-                "Added " + dieTwoValue + "!";
-        }
-        setTimeout(function () {
-            leftRolled.classList.remove("fade-in");
-            rightRolled.classList.remove("fade-in");
-        }, 1500);
-
-        */
     },
     1000
     );
